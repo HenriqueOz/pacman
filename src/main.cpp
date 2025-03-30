28dde0096614353250beb7f5cd5e84229b939651
peguinm <pedroozorio30@gmail.com>
2025-03-30 18:42:19 -0300

    remove main header

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include "main.h"
 #include "entity.h"
 #include "game.h"
 #include "ghost.h"
--- src/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MAIN_H
-#define _MAIN_H
-
-#include "SDL3/SDL.h"
-#include <cstddef>
-#include <cstdlib>
-#include <iostream>
-
-#endif
