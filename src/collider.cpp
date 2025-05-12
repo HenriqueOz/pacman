89bc2fcea9b6c117bb271250f2212005748c55b8
peguinm <pedroozorio30@gmail.com>
2025-05-11 23:57:12 -0300

    feat: implement render method for Collider

--- src/collider.cpp
+++ src/collider.cpp
@@ -15,4 +15,12 @@ Collider::update()
 void
 Collider::render(SDL_Renderer *renderer) const
 {
+    SDL_FRect rect = { static_cast<float>(m_position.x),
+                       static_cast<float>(m_position.y),
+                       static_cast<float>(m_size.x),
+                       static_cast<float>(m_size.y) };
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_RenderFillRect(renderer, &rect);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 }
